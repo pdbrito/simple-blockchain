39bfde6adba69bed802e98fcac02ac8b1daf9acc
Pedro de Brito <ped.de.brito@gmail.com>
2020-01-06 19:55:12 +0100

    Add Blockchain struct to blockchain package

---
+++ blockchain/blockchain.go
@@ -0,0 +1,19 @@
+package blockchain
+
+type Blockchain struct {
+	Blocks []*Block
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.Blocks[len(bc.Blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.Blocks = append(bc.Blocks, newBlock)
+}
+
+func NewGenesisBlock() *Block {
+	return NewBlock("A long time ago in a galaxy far far away....", []byte{})
+}
+
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
