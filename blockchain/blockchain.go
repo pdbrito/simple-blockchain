305bbceefdc9a044af040da20e7942ffc7f09e71
Pedro de Brito <ped.de.brito@gmail.com>
2020-01-08 21:41:37 +0100

    Add struct and functions to support proof of work

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,5 +1,12 @@
 package blockchain
 
+import (
+	"bytes"
+	"encoding/binary"
+	"log"
+	"math/big"
+)
+
 type Blockchain struct {
 	Blocks []*Block
 }
@@ -16,4 +23,46 @@ func newGenesisBlock() *Block {
 
 func NewBlockchain() *Blockchain {
 	return &Blockchain{[]*Block{newGenesisBlock()}}
+}
+
+const targetBits = 24
+
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+
+	pow := &ProofOfWork{b, target}
+
+	return pow
+}
+
+// IntToBytes converts an int64 to a byte array
+func IntToBytes(num int64) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, num)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return buff.Bytes()
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.Data,
+			IntToBytes(pow.block.Timestamp),
+			IntToBytes(int64(targetBits)),
+			IntToBytes(int64(nonce)),
+		},
+		[]byte{},
+	)
+
+	return data
 }
