6ec22eda295633a799f2d316fdce43c96ad9b773
Pedro de Brito <ped.de.brito@gmail.com>
2020-01-12 23:50:52 +0100

    Add Serialize method to Block

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,6 +1,9 @@
 package blockchain
 
 import (
+	"bytes"
+	"encoding/gob"
+	"log"
 	"time"
 )
 
@@ -27,4 +30,17 @@ func newBlock(data string, prevBlockHash []byte) *Block {
 	block.Nonce = nonce
 
 	return block
+}
+
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return result.Bytes()
 }
