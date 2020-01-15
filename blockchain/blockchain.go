c2fefa04500c2316d81845e0f8a715e0f0dd5e51
Pedro de Brito <ped.de.brito@gmail.com>
2020-01-15 22:52:53 +0100

    Use BoltDB to persist blockchain state
    Blockchain struct now only stores tip of the chain

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,13 +1,54 @@
 package blockchain
 
+import (
+	bolt "go.etcd.io/bbolt"
+	"log"
+)
+
+const dbFile = "blockchain.db"
+const blocksBucket = "blocks"
+
 type Blockchain struct {
-	Blocks []*Block
+	tip []byte
+	db  *bolt.DB
 }
 
 func (bc *Blockchain) AddBlock(data string) {
-	prevBlock := bc.Blocks[len(bc.Blocks)-1]
-	newBlock := newBlock(data, prevBlock.Hash)
-	bc.Blocks = append(bc.Blocks, newBlock)
+	var lastHash []byte
+
+	err := bc.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		lastHash = b.Get([]byte("1"))
+
+		return nil
+	})
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	newBlock := NewBlock(data, lastHash)
+
+	err = bc.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		err := b.Put(newBlock.Hash, newBlock.Serialize())
+		if err != nil {
+			log.Panic(err)
+		}
+
+		err = b.Put([]byte("1"), newBlock.Hash)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		bc.tip = newBlock.Hash
+
+		return nil
+	})
+
+	if err != nil {
+		log.Panic(err)
+	}
 }
 
 func newGenesisBlock() *Block {
@@ -15,5 +56,45 @@ func newGenesisBlock() *Block {
 }
 
 func NewBlockchain() *Blockchain {
-	return &Blockchain{[]*Block{newGenesisBlock()}}
+	var tip []byte
+	db, err := bolt.Open(dbFile, 0600, nil)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+
+		if b == nil {
+			genesis := newGenesisBlock()
+			b, err := tx.CreateBucket([]byte(blocksBucket))
+			if err != nil {
+				log.Panic(err)
+			}
+
+			err = b.Put(genesis.Hash, genesis.Serialize())
+			if err != nil {
+				log.Panic(err)
+			}
+
+			err = b.Put([]byte("1"), genesis.Hash)
+			if err != nil {
+				log.Panic(err)
+			}
+			tip = genesis.Hash
+		} else {
+			tip = b.Get([]byte("1"))
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bc := Blockchain{tip, db}
+
+	return &bc
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pdbrito/simple-blockchain
 
 go 1.13
+
+require go.etcd.io/bbolt v1.3.3
