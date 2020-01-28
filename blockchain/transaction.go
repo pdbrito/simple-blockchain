72609053bb6b5a471f2cdfd20ae3b5dd95e54687
Pedro de Brito <ped.de.brito@gmail.com>
2020-01-28 19:22:55 +0100

    Add NewUTXOTransaction() function

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"encoding/gob"
+	"encoding/hex"
 	"fmt"
 	"log"
 )
@@ -60,6 +61,39 @@ func NewCoinbaseTX(to, data string) *Transaction {
 	txin := TXInput{[]byte{}, -1, data}
 	txout := TXOutput{subsidy, to}
 	tx := Transaction{nil, []TXInput{txin}, []TXOutput{txout}}
+	tx.SetID()
+
+	return &tx
+}
+
+func NewUTXOTransaction(from, to string, amount int, bc *Blockchain) *Transaction {
+	var inputs []TXInput
+	var outputs []TXOutput
+
+	acc, validOutputs := bc.FindSpendableOutputs(from, amount)
+
+	if acc < amount {
+		log.Panic("ERROR: not enough funds")
+	}
+
+	// Build a list of inputs.
+	for txid, outs := range validOutputs {
+		txID, err := hex.DecodeString(txid)
+
+		if err != nil {
+			log.Panic(err)
+		}
+
+		for _, out := range outs {
+			input := TXInput{txID, out, from}
+			inputs = append(inputs, input)
+		}
+	}
+
+	// Build a list of outputs.
+	outputs = append(outputs, TXOutput{amount, to})
+
+	tx := Transaction{nil, inputs, outputs}
 	tx.SetID()
 
 	return &tx
