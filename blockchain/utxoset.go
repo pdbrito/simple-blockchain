065c96aa113d4baf91fc5ed830765ac4a90b235d
Pedro de Brito <ped.de.brito@gmail.com>
2020-02-21 22:10:21 +0100

    Add FindSpendableOutputs() method to UTXOSet

--- blockchain/utxoset.go
+++ blockchain/utxoset.go
@@ -1,6 +1,46 @@
 package blockchain
 
+import (
+	"encoding/hex"
+	bolt "go.etcd.io/bbolt"
+	"log"
+)
+
 // UTXOSet represents the UTXO set
 type UTXOSet struct {
 	Blockchain *Blockchain
+}
+
+const utxoBucket = "chainstate"
+
+// FindSpendableOutputs finds and returns unspent outputs to reference in inputs
+func (u UTXOSet) FindSpendableOutputs(pubkeyHash []byte, amount int) (int, map[string][]int) {
+	unspentOutputs := make(map[string][]int)
+	accumulated := 0
+	db := u.Blockchain.Db
+
+	err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(utxoBucket))
+		c := b.Cursor()
+
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			txID := hex.EncodeToString(k)
+			outs := DeserializeOutputs(v)
+
+			for outIdx, output := range outs.Ouputs {
+				if output.IsLockedWithKey(pubkeyHash) && accumulated < amount {
+					accumulated += output.Value
+					unspentOutputs[txID] = append(unspentOutputs[txID], outIdx)
+				}
+			}
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return accumulated, unspentOutputs
 }
