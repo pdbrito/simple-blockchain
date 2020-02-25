d5c4a7832ce0476897be1cdb19047fc0c785ca6a
Pedro de Brito <ped.de.brito@gmail.com>
2020-02-25 22:19:02 +0100

    Add Reindex() method to UTXOSet

--- blockchain/utxoset.go
+++ blockchain/utxoset.go
@@ -126,3 +126,46 @@ func (u UTXOSet) Update(block *Block) {
 		log.Panic(err)
 	}
 }
+
+// Reindex rebuilds the UTXO set
+func (u UTXOSet) Reindex() {
+	db := u.Blockchain.Db
+	bucketName := []byte(utxoBucket)
+
+	err := db.Update(func(tx *bolt.Tx) error {
+		err := tx.DeleteBucket(bucketName)
+		if err != nil && err != bolt.ErrBucketNotFound {
+			log.Panic(err)
+		}
+
+		_, err = tx.CreateBucket(bucketName)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		return nil
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+
+	UTXO := u.Blockchain.FindUTXO()
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(bucketName)
+
+		for txID, outs := range UTXO {
+			key, err := hex.DecodeString(txID)
+			if err != nil {
+				log.Panic(err)
+			}
+
+			err = b.Put(key, outs.Serialize())
+			if err != nil {
+				log.Panic(err)
+			}
+
+		}
+		return nil
+	})
+}
