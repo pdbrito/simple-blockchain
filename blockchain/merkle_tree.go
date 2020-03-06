4491b208afbfa179771ed2f4517b953f48a71e68
Pedro de Brito <ped.de.brito@gmail.com>
2020-03-06 21:35:10 +0100

    Add NewMerkleNode()

--- blockchain/merkle_tree.go
+++ blockchain/merkle_tree.go
@@ -1,5 +1,9 @@
 package blockchain
 
+import (
+	"crypto/sha256"
+)
+
 // MerkleTree represents a Merkle tree
 type MerkleTree struct {
 	RootNode *MerkleNode
@@ -10,4 +14,23 @@ type MerkleNode struct {
 	Left  *MerkleNode
 	Right *MerkleNode
 	Data  []byte
+}
+
+// NewMerkleNode creates a new Merkle tree node
+func NewMerkleNode(left, right *MerkleNode, data []byte) *MerkleNode {
+	mNode := MerkleNode{}
+
+	if left == nil && right == nil {
+		hash := sha256.Sum256(data)
+		mNode.Data = hash[:]
+	} else {
+		prevHashes := append(left.Data, right.Data...)
+		hash := sha256.Sum256(prevHashes)
+		mNode.Data = hash[:]
+	}
+
+	mNode.Left = left
+	mNode.Right = right
+
+	return &mNode
 }
