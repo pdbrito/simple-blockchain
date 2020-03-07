807fac371941cbfd4ba8517c054a7e68b93b4ed7
Pedro de Brito <ped.de.brito@gmail.com>
2020-03-07 21:50:00 +0100

    Add NewMerkleTree()

--- blockchain/merkle_tree.go
+++ blockchain/merkle_tree.go
@@ -34,3 +34,32 @@ func NewMerkleNode(left, right *MerkleNode, data []byte) *MerkleNode {
 
 	return &mNode
 }
+
+// NewMerkleTree creates a new merkle tree from a sequence of data
+func NewMerkleTree(data [][]byte) *MerkleTree {
+	var nodes []MerkleNode
+
+	if len(data)%2 != 0 {
+		data = append(data, data[len(data)-1])
+	}
+
+	for _, datum := range data {
+		node := NewMerkleNode(nil, nil, datum)
+		nodes = append(nodes, *node)
+	}
+
+	for i := 0; i < len(data)/2; i++ {
+		var newLevel []MerkleNode
+
+		for j := 0; j < len(nodes); j += 2 {
+			node := NewMerkleNode(&nodes[j], &nodes[j+1], nil)
+			newLevel = append(newLevel, *node)
+		}
+
+		nodes = newLevel
+	}
+
+	mTree := MerkleTree{&nodes[0]}
+
+	return &mTree
+}
