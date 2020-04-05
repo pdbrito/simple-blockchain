b597ee2bc50e8d41fa8d6af515d82d3dc16326b7
Pedro de Brito <ped.de.brito@gmail.com>
2020-04-05 21:10:28 +0200

    Add DeserializeTransaction()

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -216,3 +216,16 @@ func (tx *Transaction) Verify(prevTxs map[string]Transaction) bool {
 	}
 	return true
 }
+
+// DeserializeTransaction deserializes a transaction
+func DeserializeTransaction(data []byte) Transaction {
+	var transaction Transaction
+
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+	err := decoder.Decode(&transaction)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return transaction
+}
