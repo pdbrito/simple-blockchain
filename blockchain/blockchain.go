0de30dbb58b8229ce456d93f8f76b4c20da9b865
Pedro de Brito <ped.de.brito@gmail.com>
2020-04-06 20:17:58 +0200

    Add VerifyTransaction()

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -349,3 +349,23 @@ func (bc *Blockchain) AddBlock(block *Block) {
 		log.Panic(err)
 	}
 }
+
+// VerifyTransaction verifies transaction input signatures
+func (bc *Blockchain) VerifyTransaction(tx *Transaction) bool {
+	if tx.IsCoinbase() {
+		return true
+	}
+
+	prevTXs := make(map[string]Transaction)
+
+	for _, vin := range tx.Vin {
+		prevTX, err := bc.FindTransaction(vin.Txid)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		prevTXs[hex.EncodeToString(prevTX.ID)] = prevTX
+	}
+
+	return tx.Verify(prevTXs)
+}
